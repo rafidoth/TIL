4a548bdd3be14092cfedfe384957d95c01cf20a1
rafidoth <rafiulhasan803@gmail.com>
2023-11-11 23:08:31 +0600

    t-221 added

--- Java/Solve.java
+++ Java/Solve.java
@@ -13,8 +13,7 @@ public class Solve {
         while(start<=end){
             int mid = start + (end-start) /2;
             if(target==nums[mid]) return mid;
-            else if (target > nums[mid]) start = mid + 1;
-            else end = mid - 1;
+            else if (target > nums[mid]) start = mid + 1;       else end = mid - 1;
         }
 
         return -1;
